a833b884862624fa86c207571ea913ffe85cf32d
nikhil6392 <nikhilpathak210431@gmail.com>
2024-08-21 21:19:32 +0530

    Loading Name of albums

---
+++ scripts/albums/load_album_list.js
@@ -0,0 +1,31 @@
+var http=require('http');
+
+var fs=require('fs');
+
+function load_album_list(){
+    fs.readdir("album",(err,files)=>{
+        if(err){
+            
+            return err;
+        }
+         return (null,files);
+    });
+}
+
+function handle_incoming_request(req,res){
+    console.log("INCOMING REQUEST"+req.method+" "+req.url);
+
+    load_album_list((err,album)=>{
+        if(err){
+            res.writeHead(500,{"Content-Type":"application/json"});
+            res.end(JSON.stringify(err)+"\n");
+        }
+
+        var out ={error:null,data:{album:album}};
+        res.writeHead(200,{"Content-Type":"application/json"});
+        res.end(JSON.stringify(out)+"\n");
+    });
+}
+
+var s=http.createServer(handle_incoming_request);
+s.listen(8087);
